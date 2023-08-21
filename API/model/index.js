c18a0a56a72c147de5e0b3fbe61eee26c00f51b9
Samkelo <sammpokela@gmail.com>
2023-08-21 11:12:10 +0200

    first commit

---
+++ API/model/index.js
@@ -0,0 +1,7 @@
+const Users = require('./user')
+// const Products = require('./products')
+//Export all objects
+// module.exports = {
+//     users: new Users(),
+//     products: new Products()
+// }
