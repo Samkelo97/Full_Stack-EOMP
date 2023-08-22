1458d1cbf8fc5dc25806f52dd1bea442f900b941
Siya021 <siyamthandamazokwana@gmail.com>
2023-08-22 16:35:33 +0200

    save

--- fullstack_gui/src/views/ProductsView.vue
+++ fullstack_gui/src/views/ProductsView.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h2>Products</h2>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/asvML5L7Y0Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
 </template>
 
